6929207210096160bc614907081c2a647cc7603d
Hanekeko (Bilal Benyagoub) <75778663+kanekiadd@users.noreply.github.com>
2024-03-19 16:22:38 +0100

    creation page contacte v1

--- includes/components/rubrique_contact.php
+++ includes/components/rubrique_contact.php
@@ -1,2 +1,12 @@
-<h2>Nous contacter</h2>
-<h3>oû nous trouver</h3>
+<div class="container_contact">
+    <h2>Nous contacter</h2>
+    <br>
+    <h3>oû nous trouver?</h3>
+    <p> adresse: <br> 13 rue du Brade, 41000, Blois</p>
+    <img src="./assets/images/completed-google-places-label.png" alt="" class="google_map">
+    <p>numéro de telephone : <br> 06060606</p>
+    <br>
+    <p>email: <br> <a href="#">DUBRAC@GrandAngle.com</a></p>
+    
+    
+</div>
